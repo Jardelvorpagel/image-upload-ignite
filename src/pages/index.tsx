8cacf0fba1994e247c2e519db2dd4fbd3a5d43bc
Jardelvorpagel <jardelvorpagel@gmail.com>
2022-09-16 01:21:56 -0300

    fix list behavior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import { CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
-import { ImagesQueryResponse } from './api/images';
 
 export default function Home(): JSX.Element {
   const fetchImages = async ({ pageParam = null }): Promise<any> => {
@@ -32,17 +31,7 @@ export default function Home(): JSX.Element {
   });
 
   const formattedData = useMemo(() => {
-    const flattenedData = data?.pages.flat()[0].data;
-
-    console.log(flattenedData);
-
-    // return flattenedData?.map(({ data: dataQuery, ts, ref }) => ({
-    //   title: dataQuery?.title,
-    //   description: dataQuery?.description,
-    //   url: dataQuery?.url,
-    //   ts,
-    //   id: ref?.id,
-    // }));
+    const flattenedData = data?.pages.map(page => page.data).flat();
 
     if (!flattenedData) return [];
 
@@ -55,8 +44,6 @@ export default function Home(): JSX.Element {
     }));
   }, [data]);
 
-  console.log('formattedData', formattedData);
-
   if (isLoading) return <Loading />;
 
   if (isError) return <Error />;
@@ -68,7 +55,11 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {hasNextPage && (
-          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+          <Button
+            mt="40px"
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+          >
             {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
           </Button>
         )}
